d1b899f71ab2c975637ae77619b0ea0175adc06c
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-12-16 17:50:48 -0500

    Refactor add function and add new typed function interface

--- functions.ts
+++ functions.ts
@@ -2,7 +2,16 @@
 type typedFunction =  (x: number, y: number) => number;
 
 const add: typedFunction = (x, y) => {
-  return x + y
+    return x + y
 }
 
 console.log(add(1, 2))
+
+interface TypedFunctionInterface {
+    (x: number, y: number): number
+}
+const add1: TypedFunctionInterface = (x, y) => {
+    return x + y
+}
+
+console.log(add1(1, 2))
