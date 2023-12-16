5a79a676c4f429339b9163404a8eaac64706fcba
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-12-16 16:28:54 -0500

    Add string variable, strict equality check, and reading from stdin

--- practice.ts
+++ practice.ts
@@ -1,9 +1,26 @@
+
+// string
 let fullName: string = 'Bob Smith';
 fullName = 'John Doe';
 console.log(`Hello, ${fullName}`);
 
+
+// strict equality
 let a: string = "abc"
 
 if (a === "abc") {
     console.log("a is abc")
 }
+
+// reading from stdin
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter a number: ', (answer: string) => {
+  console.log(`You entered: ${answer}`);
+  rl.close();
+});
