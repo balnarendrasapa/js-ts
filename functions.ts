1fa0b36be68aa2a95ddb8d97b4269c6a39f23e3a
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-12-16 19:16:51 -0500

    Update function calls and add new functions

--- functions.ts
+++ functions.ts
@@ -5,7 +5,7 @@ const add: typedFunction = (x, y) => {
     return x + y
 }
 
-console.log(add(1, 2))
+console.log(add(10, 2))
 
 interface TypedFunctionInterface {
     (x: number, y: number): number
@@ -14,4 +14,17 @@ const add1: TypedFunctionInterface = (x, y) => {
     return x + y
 }
 
-console.log(add1(1, 2))
+console.log(add1(1, 20))
+
+
+const func = function (x: number, y: number): number {
+    return x + y
+}
+
+console.log(func(11, 2))
+
+const add3 = (x: number, y: number): number => {
+    return x + y
+}
+
+console.log(add3(1, 22))
