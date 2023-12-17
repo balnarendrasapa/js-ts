c9deace52086699eac719793674b68a0fd18aa5b
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-12-16 19:25:23 -0500

    Add new class and update existing code

--- classes.ts
+++ classes.ts
@@ -1,3 +1,4 @@
+// cannot use the keyword 'new' in this case.
 const student = {
     name: 'John',
     age: 30,
@@ -8,3 +9,21 @@ const student = {
 }
 
 console.log(student.printName());
+
+// can use the keyword 'new' in this case.
+
+class Student {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+    printName() {
+        console.log(this.name);
+        return this.name;
+    }
+}
+
+const student1 = new Student('John', 30);
+console.log(student1.printName());
