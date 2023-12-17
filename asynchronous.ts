c476e035969e5ef213ea9577cf989d69d85f70ff
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-12-16 20:24:08 -0500

    Add asynchronous.ts file with getData function and greet function

---
+++ asynchronous.ts
@@ -0,0 +1,24 @@
+function getData(id: number){
+    const prom = new Promise<void>((resolve) => { 
+        setTimeout(() => { 
+            console.log('Async task started '+ id);
+            resolve(); 
+        }, 4000);
+    });
+    return prom;
+}
+
+const temp = getData(1);
+
+function greet(name:string, greeting=undefined) {
+    if (name === undefined || greeting === undefined) {
+        console.log('One or more parameters are undefined');
+    } else {
+        console.log(`${greeting}, ${name}!`);
+    }
+}
+
+greet('John');
+temp.then(() => {});
+
+export { };
